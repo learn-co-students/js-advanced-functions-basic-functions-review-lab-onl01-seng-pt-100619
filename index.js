156ec758b08aea2b5c98e10de5691f274e224fa3
ZumbaIdol <david.a.foote@gmail.com>
2020-05-20 17:02:23 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // Your code here
-function saturdayFun(str, activity = roller-skate) {
+function saturdayFun(str= 'roller-skate') {
     return `This Saturday, I want to ${str}!`
 }
 
+let mondayWork = function(str= 'go to the office') {
+    return `This Monday, I will ${str}`
+}
