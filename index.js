a416831044873f39d8e56970efae112514c32ec3
Amber Taranto <ambzzz@gmail.com>
2020-06-02 21:58:20 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`);
+}
+saturdayFun("leave the house")
+
+function mondayWork(activity = "go to the office") {
+    return (`This Monday, I will ${activity}.`);
+}
+mondayWork("ditch work")
+
+function wrapAdjective(emphasizer = "*") {
+    return function (adjective = "special") {
+        return `You are ${emphasizer}${adjective}${emphasizer}!`
+    }
+}
+// let encouragePromptFunction = wrapAdjective("!!!")
+
+var Calculator = {
+    add: function (a, b) {
+        return a + b
+    },
+    subtract: function (a, b) {
+        return a - b
+    },
+    multiply: function (a, b) {
+        return a * b
+    },
+    divide: function (a, b) {
+        return a / b
+    }
+
+}
+
+function actionApplyer(integer, functions) {
+    functions.forEach(function (func) {
+        integer = func(integer)
+    })
+    return integer
+}
